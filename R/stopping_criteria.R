674c86e84167016f262d133df44fae4373651e6c
E. F. Haghish <haghish.e.f@gmail.com>
2023-05-08 16:50:02 +0200

    0.1.2

--- R/stopping_criteria.R
+++ R/stopping_criteria.R
@@ -1,73 +1,47 @@
 #' @title Stopping Criteria for Ending the Search
 #' @description Defines criteria for ending the optimization search
+#' @param df data.frame. includes the metrics of ensemblem model performance
+#' @param round integer. the current round of optimization
+#' @param stop integer. current round of stopping penalty
+#' @param min_improvement numeric. specifies the minimum improvement in model
+#'                        evaluation metric to qualify further optimization search.
 #' @param stop_rounds integer. number of stoping rounds, in case the model stops
 #'                    improving
 #' @param reset_stop_rounds logical. if TRUE, everytime the model improves the
 #'                          stopping rounds penalty is resets to 0.
 #' @param stop_metric character. model stopping metric. the default is \code{"auc"},
 #'                    but \code{"aucpr"} and \code{"mcc"} are also available.
-#' @param stop integer. current round of stopping penalty
+
 #' @return a matrix of F-Measures for different thresholds or the highest F-Measure value
 #' @author E. F. Haghish
 
-
-
-stopping_criteria <- function(stop_rounds = 3,
+stopping_criteria <- function(df,
+                              round,
+                              stop,
+                              min_improvement,
+                              stop_rounds = 3,
                               reset_stop_rounds = TRUE,
-                              stop_metric = "auc",
-                              stop = STOP) {
+                              stop_metric = "auc") {
 
-  AUC       <- 0
-  AUCPR     <- 0
-  MCC       <- 0
-  STOP      <- 0
-  STOPauc   <- 0
-  STOPaucpr <- 0
-  STOPmcc   <- 0
+  IMPROVED  <- FALSE
 
-  # evaluate the model for AUC
+  # if there is only one stopping criterion, stop the search
+  #    when the 'stop_rounds' are reached without any improvement
+  #    if several stopping criteria, stop the search
+  #    when the 'stop_rounds' are reached without any improvement
+  #    for any of the criterion
   # ----------------------------------------------------------
-  if ("auc" %in% stop_metric) {
-    auc <- as.numeric(h2o::h2o.auc(stopModel))
-    if (AUC < auc) {
-      AUC <- auc
-      TOP <- i
-      model <- stopModel
-      if (reset_stop_rounds) STOP <- 0
-    }
-    else STOPauc <- STOPauc + 1
+  for (i in stop_metric) {
+    if (df[round, i] > (df[round, i-1]+ min_improvement)) IMPROVED <- TRUE
   }
 
-  # evaluate the model for AUCPR
+  # evaluate stopping criteria
   # ----------------------------------------------------------
-  if ("aucpr" %in% stop_metric) {
-    aucpr <- as.numeric(h2o::h2o.aucpr(stopModel))
-    if (AUCPR < aucpr) {
-      AUCPR <- aucpr
-      TOP <- i
-      model <- stopModel
-      if (reset_stop_rounds) STOP <- 0
-    }
-    else STOPaucpr <- STOPaucpr + 1
-  }
+  if (IMPROVED & reset_stop_rounds) STOP <- 0
+  else if (!IMPROVED) STOP <- STOP + 1
 
-  # evaluate the model for MCC
-  # ----------------------------------------------------------
-  if ("mcc" %in% stop_metric) {
-    mcc <- max(h2o::h2o.mcc(stopModel)[,2])
-    if (MCC < mcc) {
-      MCC <- mcc
-      TOP <- i
-      model <- stopModel
-      if (reset_stop_rounds) STOP <- 0
-    }
-    else STOPmcc <- STOPmcc + 1
-  }
-
-  # Create a data.frame of all available criteria
-  # ----------------------------------------------------------
-
-  return(STOP)
+  return(list(stop = STOP,
+              improved = IMPROVED))
 }
 
 
