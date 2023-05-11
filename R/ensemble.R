cd92623c10b9f45a2c341549c8f1f97cc62c9466
E. F. Haghish <haghish.e.f@gmail.com>
2023-05-11 09:31:13 +0200

    Update ensemble.R

--- R/ensemble.R
+++ R/ensemble.R
@@ -114,8 +114,8 @@
 #' #######################################################
 #' h2o.auc(aml@leader)                          # best model identified by h2o.automl
 #' h2o.auc(h2o.getModel(grid@model_ids[[1]]))   # best model identified by grid search
-#' h2o.auc(top).                                # ensemble model with 'top' search strategy
-#' h2o.auc(search).                             # ensemble model with 'search' search strategy
+#' h2o.auc(top$model).                          # ensemble model with 'top' search strategy
+#' h2o.auc(search$model).                       # ensemble model with 'search' search strategy
 #'
 #' }
 #' @export
